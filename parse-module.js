9b22d1168500e453ff6d6106276b886eae849144
Luttennet <lottan.horev@gmail.com>
2020-09-15 16:48:32 +0300

    small debuging

--- parse-module.js
+++ parse-module.js
@@ -1,13 +1,33 @@
-var docparser = require('docparser-node');
+const docparser = require('docparser-node')
+const Module = function (filePath){
+  const client = new docparser.Client("8ede9301b2dce0bc655ce576b96bc7c8c965150f")
+  const parserId = 'njnxhzavldku'
+  const documentId = ''
+  const fullDisclosure = {
+    yield,
+    dangerLvl,
+    monthFee,
+    savesFee
+  }
 
-var client = new docparser.Client("8ede9301b2dce0bc655ce576b96bc7c8c965150f");
-const parserId = 'njnxhzavldku'
-const documentId = 'f2d0dd5fd4ea660efa366d5ea3bda54a'
+  client.fetchDocumentFromURL(parserId, filePath, {remote_id: 'test'})
+    .then(function (result) {
+      documentId = result.id
+   })
+    .catch(function (err) {
+      console.log(err)
+    })
 
-client.getResultsByDocument(parserId, documentId, {format: 'object'})
-  .then(function (result) {
-    console.log(result)
-  })
-  .catch(function (err) {
-    console.log(err)
-  })
+  client.getResultsByDocument(parserId, documentId, {format: 'object'})
+    .then(function (result) {
+      fullDisclosure.yield = result[0].yield
+      fullDisclosure.dangerLvl = result[0].dangerLvl
+      fullDisclosure.monthFee = result[0].monthFee
+      fullDisclosure.savesFee = result[0].savesFee
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+    return fullDisclosure
+}
+  module.exports = fullDisclosure
